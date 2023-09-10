124e4e776271343bbd01f0ce0f3458a2f83eb126
Alex Kazik <alex@kazik.de>
2023-09-10 19:15:07 +0200

    Add icons to to the project

--- packages/yew-bootstrap/src/lib.rs
+++ packages/yew-bootstrap/src/lib.rs
@@ -1,6 +1,10 @@
 #![doc = include_str!("../README.md")]
 
+#![forbid(unsafe_code)]
+
 /// Components supported by this crate are listed here
 pub mod component;
 /// Helper functions and types are listed here, for example [util::Color] and the [util::include_cdn] function.
 pub mod util;
+/// Access to bootstrap-icons.
+pub mod icons;
